dbf0873e2406633ab2ed6f4f1d32ab91c69b6f7f
Feras Alkilani <65820163+feras-alkilani@users.noreply.github.com>
2021-06-24 23:13:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,436 @@
+let data = [
+	{
+		"id": 1,
+		"name": "Ahmed Bustani",
+		"assistant": true,
+		"group": "Orange"
+	},
+	{
+		"id": 2,
+		"name": "Baris Tunar",
+		"assistant": true,
+		"group": "Crimson"
+	},
+	{
+		"id": 3,
+		"name": "Burak Caglar",
+		"assistant": true,
+		"group": "Khaki"
+	},
+	{
+		"id": 4,
+		"name": "ah. fawad",
+		"assistant": true,
+		"group": "Pink"
+	},
+	{
+		"id": 5,
+		"name": "Furkan KILIÇER",
+		"assistant": true,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 6,
+		"name": "Alperen Aslan",
+		"assistant": true,
+		"group": "Turquoise"
+	},
+	{
+		"id": 7,
+		"name": "joudi bitar",
+		"assistant": true,
+		"group": "Yellow"
+	},
+	{
+		"id": 8,
+		"name": "Kübra Özdem",
+		"assistant": true,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 9,
+		"name": "Aycan",
+		"assistant": true,
+		"group": "Fuscia"
+	},
+	{
+		"id": 10,
+		"name": "Aykut Şahin",
+		"assistant": true,
+		"group": "Puce"
+	},
+	{
+		"id": 11,
+		"name": "S. Zahit ACAR",
+		"assistant": true,
+		"group": "Maroon"
+	},
+	{
+		"id": 12,
+		"name": "Tugay Türk",
+		"assistant": true,
+		"group": "Purple"
+	},
+	{
+		"id": 13,
+		"name": "Abdurrahman Seyidoğlu",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 14,
+		"name": "Ahmed Tabşo",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 15,
+		"name": "Alaa Alaff",
+		"assistant": false,
+		"group": "Fuscia"
+	},
+	{
+		"id": 16,
+		"name": "ASSKOUR Imane",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 17,
+		"name": "Aydoğan Gökberk",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 18,
+		"name": "BAHADDİN ALMUHAMMED",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 19,
+		"name": "Büşra Bilgin",
+		"assistant": false,
+		"group": "Maroon"
+	},
+	{
+		"id": 20,
+		"name": "Chimnaz Vahabzade",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 21,
+		"name": "Çağatay Sarıoğlan",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 22,
+		"name": "dana alhameed",
+		"assistant": false,
+		"group": "Puce"
+	},
+	{
+		"id": 23,
+		"name": "Delfin Öztürk",
+		"assistant": false,
+		"group": "Purple"
+	},
+	{
+		"id": 24,
+		"name": "Ebubekir Karataş",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 25,
+		"name": "Ela Yağmur ",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 26,
+		"name": "ELNAZ HAMEDIBAZAZ",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 27,
+		"name": "Fatma Şehitoğlu",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 28,
+		"name": "Feras Alkilani",
+		"assistant": false,
+		"group": "Fuscia"
+	},
+	{
+		"id": 29,
+		"name": "Gizem Nur Korkmaz",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 30,
+		"name": "Halil İbrahim Badem",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 31,
+		"name": "Hasan ALKHALAF",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 32,
+		"name": "Hatice Sürümlü",
+		"assistant": false,
+		"group": "Purple"
+	},
+	{
+		"id": 33,
+		"name": "IMAN ALABID",
+		"assistant": false,
+		"group": "Maroon"
+	},
+	{
+		"id": 34,
+		"name": "Işıl Özkaynak",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 35,
+		"name": "İbrahim Halil DÖŞ",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 36,
+		"name": "İlkay Çitak",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 37,
+		"name": "Kenan Mirzeyev",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 38,
+		"name": "Melisa Gürdal",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 39,
+		"name": "Merve Şahin",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 40,
+		"name": "MHD NABIL ARAR",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 41,
+		"name": "Muhammet BİLGİN",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 42,
+		"name": "Nurgeldi Atamyradov",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 43,
+		"name": "Ozan Ali TÜRKYILMAZ",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 44,
+		"name": "Özge Yurdusev",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 45,
+		"name": "Parahat Nepesov",
+		"assistant": false,
+		"group": "Puce"
+	},
+	{
+		"id": 46,
+		"name": "Rined haj osman",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 47,
+		"name": "Serkan Çelik",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 48,
+		"name": "Sinem Nurioğlu",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 49,
+		"name": "Şifa Hasani",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 50,
+		"name": "Yasemin Paşaoğlu",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 51,
+		"name": "Yusuf sabbagh",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 52,
+		"name": "Nurgeldi Atamyradov",
+		"assistant": false,
+		"group": "Yellow"
+	}
+];
+
+
+
+data.forEach(myFunction_yellow);
+
+function myFunction_yellow(item) {
+ 
+if ( item.group == "Yellow")
+    document.getElementById("demo_yellow").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+  
+  data.forEach(myFunction_aquamarine);
+
+function myFunction_aquamarine(item) {
+ 
+if ( item.group == "Aquamarine")
+  document.getElementById("demo_aquamarine").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+
+data.forEach(myFunction_turquoise);
+
+function myFunction_turquoise(item) {
+ 
+if ( item.group == "Turquoise")
+ 
+    document.getElementById("demo_turquoise").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+
+data.forEach(myFunction_crimson);
+
+function myFunction_crimson(item) {
+ 
+if ( item.group == "Crimson")
+ 
+ document.getElementById("demo_crimson").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+
+
+data.forEach(myFunction_puce);
+function myFunction_puce(item) {
+ 
+if ( item.group == "Puce")
+ 
+    document.getElementById("demo_puce").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+
+////////////////////////
+
+data.forEach(myFunction_Goldenrod);
+function myFunction_Goldenrod(item) {
+ 
+if ( item.group == "Goldenrod")
+ 
+    document.getElementById("demo_Goldenrod").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+data.forEach(myFunction_Pink);
+function myFunction_Pink(item) {
+ 
+if ( item.group == "Pink")
+ 
+    document.getElementById("demo_Pink").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+
+data.forEach(myFunction_Khaki);
+function myFunction_Khaki(item) {
+ 
+if ( item.group == "Khaki")
+ 
+    document.getElementById("demo_Khaki").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+
+data.forEach(myFunction_Orange);
+function myFunction_Orange(item) {
+ 
+if ( item.group == "Orange")
+ 
+    document.getElementById("demo_Orange").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+
+data.forEach(myFunction_Maroon);
+function myFunction_Maroon(item) {
+ 
+if ( item.group == "Maroon")
+ 
+    document.getElementById("demo_Maroon").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+data.forEach(myFunction_Purple);
+function myFunction_Purple(item) {
+ 
+if ( item.group == "Purple")
+ 
+    document.getElementById("demo_Purple").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+data.forEach(myFunction_Fuscia);
+function myFunction_Fuscia(item) {
+ 
+if ( item.group == "Fuscia")
+ 
+    document.getElementById("demo_Fuscia").innerHTML += item.id +  ":" +'   '  + item.name +"---"  +  item.group + "---"    + '  ' + item.assistant + "<br>";
+   }
+/////////////////////////
+
+
+ 
+  
+
+  
+
+
+
+
+ 
+
+  
+
+
